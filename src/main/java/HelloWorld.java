c251f188f4895977e2c1a7832d59f3f252f9829e
venugar <Arun-Raj.VENUGOPAL@ext.ec.europa.eu>
2019-05-28 14:36:37 +0200

    Initializing errorCollectorRetry project with basic Maven setup

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author Arun Raj
+ * @version 1.0
+ * @since 28/05/2019
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/TestHelloWorld.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author Arun Raj
+ * @version 1.0
+ * @since 28/05/2019
+ */
+public class TestHelloWorld {
+    @Test
+    public void testMain(){
+        HelloWorld.main(new String [1] );
+    }
+}
